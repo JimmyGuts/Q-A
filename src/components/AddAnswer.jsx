b36bb100ea15def99a228305345b8043741b1164
Andre Thomas <andrethomas6@yahoo.com>
2020-08-12 12:53:14 -0700

    Refactored  AddAnswer and AddQuestion to perform field validation. Values are kept in components state. Updated the cancel and submit buttons in the modal.

--- src/components/AddAnswer.jsx
+++ src/components/AddAnswer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Link } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -42,15 +42,53 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 //    e. images selected are invalid or unable to be uploaded
 
 const AddAnswer = () => {
+  // Functions to handle Modal Open, Close, Cancel
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
+  const handleCancel = () => {
+    resetFields();
+    setOpen(false);
+  };
+
+  // Function to reset state values
+  const resetFields = () => {
+    updateName("");
+    updateEmail("");
+    updateBody("");
+    setErrors({});
+  };
+
+  // Function to handle field validation
+  const validate = () => {
+    let temp = {};
+    temp.name = name ? "" : "The Name field is required.";
+    temp.email = /^\S+@\S+\.\S+$/.test(email) ? "" : "Email is not valid and is required.";
+    temp.body = body ? "" : "The Answer field is required.";
+    setErrors({ ...temp });
+    return Object.values(temp).every((field) => field === "");
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    if (validate()) {
+      // window.alert("testing...");
+
+      resetFields();
+      handleClose();
+    }
+  };
+
+  // States that hold form values
+  const [name, updateName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [body, updateBody] = useState("");
+  const [errors, setErrors] = useState({});
 
   return (
     <div>
@@ -62,10 +100,10 @@ const AddAnswer = () => {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Add a Question</DialogTitle>
+        <DialogTitle id="form-dialog-title">Add a Answer</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To add a answer to this question please enter a nickname and email
+            To add a answer to this question please enter your name and email
             address along with your answer.
           </DialogContentText>
           <TextField
@@ -75,9 +113,17 @@ const AddAnswer = () => {
             type="text"
             fullWidth
             required
+            defaultValue={name}
+            onChange={(event) => {
+              updateName(event.target.value);
+            }}
+            {...(errors.name && { error: true, helperText: errors.name })}
+            {...(!errors.name && {
+              helperText:
+                "For privacy reasons, do not use your full name or email address",
+            })}
             inputProps={{ maxLength: 60 }}
             placeholder="Example: jack543!"
-            helperText="For privacy reasons, do not use your full name or email address"
           />
           <TextField
             margin="dense"
@@ -86,26 +132,37 @@ const AddAnswer = () => {
             type="email"
             fullWidth
             required
+            defaultValue={email}
+            onChange={(event) => {
+              updateEmail(event.target.value);
+            }}
+            {...(errors.email && { error: true, helperText: errors.email })}
+            {...(!errors.email && {
+              helperText: "For authentication reasons, you will not be emailed",
+            })}
             inputProps={{ maxLength: 60 }}
             placeholder="Example: jack@email.com"
-            helperText="For authentication reasons, you will not be emailed"
           />
           <TextField
             id="standard-textarea"
             label="Answer"
-            rows={2}
             fullWidth
             multiline
             required
+            defaultValue={body}
+            onChange={(event) => {
+              updateBody(event.target.value);
+            }}
+            {...(errors.body && { error: true, helperText: errors.body })}
             inputProps={{ maxLength: 1000 }}
             placeholder="What is your answer to this question?"
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="inherit">
+          <Button onClick={handleCancel} color="inherit">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="inherit">
+          <Button onClick={handleSubmit} color="inherit">
             Submit Answer
           </Button>
         </DialogActions>
@@ -113,5 +170,176 @@ const AddAnswer = () => {
     </div>
   );
 };
+
+//   const [open, setOpen] = useState(false);
+
+//   // States that hold form values
+//   const [name, updateName] = useState("");
+//   const [email, updateEmail] = useState("");
+//   const [body, updateBody] = useState("");
+
+//   // Are fields valid
+//   const [nameIsValid, nameValidation] = useState(true);
+//   const [emailIsValid, emailValidation] = useState(true);
+//   const [bodyIsValid, bodyValidation] = useState(true);
+
+//   // Toggles normal and error forms
+//   const validateForm = () => {
+//     name ? nameValidation(true) : nameValidation(false);
+//     email ? emailValidation(true) : emailValidation(false);
+//     body ? bodyValidation(true) : bodyValidation(false);
+//   };
+
+//   const handleSubmit = () => {
+//     validateForm();
+//     if (nameIsValid && emailIsValid && bodyIsValid) {
+//       // Submit Form
+//       // Clear State
+//       updateName("");
+//       updateEmail("");
+//       updateBody("");
+//     }
+//   };
+
+//   // Handlers to Open and Close form
+//   const handleClickOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <div>
+//       <Link variant="caption" color="inherit" onClick={handleClickOpen}>
+//         Add Answer
+//       </Link>
+//       <Dialog
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="form-dialog-title"
+//       >
+//         <DialogTitle id="form-dialog-title">Add a Question</DialogTitle>
+//         <DialogContent>
+//           <DialogContentText>
+//             To add a answer to this question please enter a nickname and email
+//             address along with your answer.
+//           </DialogContentText>
+//           {nameIsValid ? (
+//             <TextField
+//               margin="dense"
+//               id="name"
+//               label="Name"
+//               type="text"
+//               error={false}
+//               defaultValue={name}
+//               onChange={(event) => {
+//                 updateName(event.target.value);
+//               }}
+//               fullWidth
+//               required
+//               inputProps={{ maxLength: 60 }}
+//               placeholder="Example: jack543!"
+//               helperText="For privacy reasons, do not use your full name or email address"
+//             />
+//           ) : (
+//             <TextField
+//               margin="dense"
+//               id="name"
+//               label="Name"
+//               type="text"
+//               defaultValue={name}
+//               error
+//               onChange={(event) => {
+//                 updateName(event.target.value);
+//               }}
+//               fullWidth
+//               required
+//               inputProps={{ maxLength: 60 }}
+//               placeholder="Example: jack543!"
+//               helperText="You must enter a Name"
+//             />
+//           )}
+//           {emailIsValid ? (
+//             <TextField
+//               margin="dense"
+//               id="email"
+//               label="Email Address"
+//               type="email"
+//               defaultValue={email}
+//               onChange={(event) => {
+//                 updateEmail(event.target.value);
+//               }}
+//               fullWidth
+//               required
+//               inputProps={{ maxLength: 60 }}
+//               placeholder="Example: jack@email.com"
+//               helperText="For authentication reasons, you will not be emailed"
+//             />
+//           ) : (
+//             <TextField
+//               margin="dense"
+//               id="email"
+//               label="Email Address"
+//               type="email"
+//               defaultValue={email}
+//               error
+//               onChange={(event) => {
+//                 updateEmail(event.target.value);
+//               }}
+//               fullWidth
+//               required
+//               inputProps={{ maxLength: 60 }}
+//               placeholder="Example: jack@email.com"
+//               helperText="You must enter an email address"
+//             />
+//           )}
+//           {bodyIsValid ? (
+//             <TextField
+//               id="standard-textarea"
+//               label="Answer"
+//               defaultValue={body}
+//               onChange={(event) => {
+//                 updateBody(event.target.value);
+//               }}
+//               rows={2}
+//               fullWidth
+//               multiline
+//               required
+//               inputProps={{ maxLength: 1000 }}
+//               placeholder="What is your answer to this question?"
+//             />
+//           ) : (
+//             <TextField
+//               id="standard-textarea"
+//               label="Answer"
+//               defaultValue={body}
+//               error
+//               onChange={(event) => {
+//                 updateBody(event.target.value);
+//               }}
+//               rows={2}
+//               fullWidth
+//               multiline
+//               required
+//               inputProps={{ maxLength: 1000 }}
+//               placeholder="What is your answer to this question?"
+//               helperText="You must enter an answer."
+//             />
+//           )}
+//         </DialogContent>
+//         <DialogActions>
+//           <Button onClick={handleClose} color="inherit">
+//             Cancel
+//           </Button>
+//           <Button onClick={handleSubmit} color="inherit">
+//             Submit Answer
+//           </Button>
+//         </DialogActions>
+//       </Dialog>
+//     </div>
+//   );
+// };
 
 export default AddAnswer;
--- src/components/AddQuestion.jsx
+++ src/components/AddQuestion.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -36,15 +36,54 @@ import AddIcon from "@material-ui/icons/Add";
 //    d. field is invalid if any required field is blank or email is not in correct format
 
 const AddQuestion = () => {
+  // Function to handle Modal Open, Close, Cancel
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
+  const handleCancel = () => {
+    resetFields();
+    setOpen(false);
+  };
+  // Function to reset state values
+  const resetFields = () => {
+    updateName("");
+    updateEmail("");
+    updateBody("");
+    setErrors({});
+  };
+
+  // Function to handle field validation
+  const validate = () => {
+    let temp = {};
+    temp.name = name ? "" : "The Name field is required.";
+    temp.email = /^\S+@\S+\.\S+$/.test(email)
+      ? ""
+      : "Email is not valid and is required.";
+    temp.body = body ? "" : "The Question field is required.";
+    setErrors({ ...temp });
+    return Object.values(temp).every((field) => field === "");
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault;
+    if (validate()) {
+      // window.alert("testing...");
+      
+      resetFields();
+      handleClose();
+    }
+  };
+
+  // States that hold form values
+  const [name, updateName] = useState("");
+  const [email, updateEmail] = useState("");
+  const [body, updateBody] = useState("");
+  const [errors, setErrors] = useState({});
 
   return (
     <div>
@@ -74,9 +113,17 @@ const AddQuestion = () => {
             type="text"
             fullWidth
             required
+            defaultValue={name}
+            onChange={(event) => {
+              updateName(event.target.value);
+            }}
+            {...(errors.name && { error: true, helperText: errors.name })}
+            {...(!errors.name && {
+              helperText:
+                "For privacy reasons, do not use your full name or email address",
+            })}
             inputProps={{ maxLength: 60 }}
             placeholder="Example: jackson11!"
-            helperText="For privacy reasons, do not use your full name or email address"
           />
           <TextField
             margin="dense"
@@ -85,26 +132,37 @@ const AddQuestion = () => {
             type="email"
             fullWidth
             required
+            defaultValue={email}
+            onChange={(event) => {
+              updateEmail(event.target.value);
+            }}
+            {...(errors.email && { error: true, helperText: errors.email })}
+            {...(!errors.email && {
+              helperText: "For authentication reasons, you will not be emailed",
+            })}
             inputProps={{ maxLength: 60 }}
             placeholder="Example: jack@email.com"
-            helperText="For authentication reasons, you will not be emailed"
           />
           <TextField
             id="standard-textarea"
             label="Question"
-            rows={2}
             fullWidth
             multiline
             required
+            defaultValue={body}
+            onChange={(event) => {
+              updateBody(event.target.value);
+            }}
+            {...(errors.body && { error: true, helperText: errors.body })}
             inputProps={{ maxLength: 1000 }}
             placeholder="What is your question about the product?"
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="inherit">
+          <Button onClick={handleCancel} color="inherit">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="inherit">
+          <Button onClick={handleSubmit} color="inherit">
             Submit Question
           </Button>
         </DialogActions>
